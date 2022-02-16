d72925cbf85cfdfb3c9330a9f9384b149b6980bd
Lam Do <tunglamdo96@gmail.com>
2022-02-16 23:32:48 +0700

    them socket io

--- chat_app/App.js
+++ chat_app/App.js
@@ -1,112 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useEffect} from 'react';
+import {Text, View, TextInput} from 'react-native';
+import io from 'socket.io-client';
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [socket, setSocket] = useState();
+  const [chatMessages, setChatMessage] = useState([]);
+  useEffect(() => {
+    setSocket(io('http://192.168.31.238:3000'));
+  }, []);
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  useEffect(() => {
+    if (socket === undefined) {
+      return;
+    }
+    socket.on('chat_message', msg => {
+      console.log('socketqqqqq', chatMessages);
+      setChatMessage([...chatMessages, chatMessages.push(msg)]);
+    });
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket]);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onSubmitText = text => {
+    socket.emit('chat_message', message);
+    setMessage('');
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{padding: 16}}>
+      <View
+        style={{
+          borderRadius: 8,
+          height: 50,
+          borderColor: '#cc8',
+          justifyContent: 'center',
+          borderWidth: 0.5,
+          paddingLeft: 10,
+        }}>
+        <TextInput
+          placeholder="...."
+          value={message}
+          onChangeText={text => setMessage(text)}
+          onSubmitEditing={txt => onSubmitText(txt)}
+          style={{color: '#000', fontSize: 14}}
+        />
+      </View>
+
+      <View style={{marginTop: 10}}>
+        {chatMessages.map((item, index) => (
+          <Text
+            key={index.toString()}
+            style={{margin: 6, color: '#fcc', fontSize: 14}}>
+            test_{item}
+          </Text>
+        ))}
+      </View>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
