7652cb01ecaa140c1e02b2d74bee43bf02392b81
Đỗ Tùng Lâm <lamdt16@viettel.com.vn>
2022-02-18 15:44:26 +0700

    thay doi message

--- chat_app/App.js
+++ chat_app/App.js
@@ -1,24 +1,25 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState, useEffect} from 'react';
-import {Text, View, TextInput} from 'react-native';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 import io from 'socket.io-client';
 const App = () => {
   const [message, setMessage] = useState('');
   const [socket, setSocket] = useState();
-  const [chatMessages, setChatMessage] = useState([]);
+  const [listMessage, setListMessage] = useState([]);
   useEffect(() => {
-    setSocket(io('http://192.168.31.238:3000'));
+    setSocket(io('http://172.16.103.155:3000'));
   }, []);
 
   useEffect(() => {
     if (socket === undefined) {
       return;
     }
-    socket.on('chat_message', msg => {
-      console.log('socketqqqqq', chatMessages);
-      setChatMessage([...chatMessages, chatMessages.push(msg)]);
+    socket.on('chat_message', txt => {
+      const data = [txt].concat(listMessage);
+      setListMessage(data);
     });
-    // // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
 
   const onSubmitText = text => {
@@ -27,34 +28,58 @@ const App = () => {
   };
 
   return (
-    <View style={{padding: 16}}>
+    <View style={{padding: 16, flex: 1}}>
       <View
         style={{
           borderRadius: 8,
           height: 50,
-          borderColor: '#cc8',
+          borderColor: '#2b2b2b',
           justifyContent: 'center',
           borderWidth: 0.5,
           paddingLeft: 10,
         }}>
         <TextInput
-          placeholder="...."
+          placeholder="input here"
+          placeholderTextColor="#a3a3a3"
           value={message}
           onChangeText={text => setMessage(text)}
-          onSubmitEditing={txt => onSubmitText(txt)}
-          style={{color: '#000', fontSize: 14}}
+          style={{
+            color: '#000',
+            fontStyle: 'italic',
+            fontSize: 14,
+          }}
         />
       </View>
 
       <View style={{marginTop: 10}}>
-        {chatMessages.map((item, index) => (
+        {listMessage.map((item, index) => (
           <Text
             key={index.toString()}
-            style={{margin: 6, color: '#fcc', fontSize: 14}}>
-            test_{item}
+            style={{
+              margin: 6,
+              color: '#ff637d',
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}>
+            {index}: {item}
           </Text>
         ))}
       </View>
+
+      <TouchableOpacity
+        onPress={() => onSubmitText(message)}
+        style={{
+          backgroundColor: 'black',
+          position: 'absolute',
+          bottom: 0,
+          height: 45,
+          left: 0,
+          right: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 14, color: '#fff'}}>ADD TEXT</Text>
+      </TouchableOpacity>
     </View>
   );
 };
